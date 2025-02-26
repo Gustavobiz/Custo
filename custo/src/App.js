5a5677e3b70953e8fefd5352e144fb2df2546885
Gustavobiz <gustavobernardes05@hotmail.com>
2025-02-26 17:22:01 -0300

    feat: navbar

--- custo/src/App.js
+++ custo/src/App.js
@@ -4,18 +4,14 @@ import Home from "./components/pages/Home";
 import NewProject from "./components/pages/NewProject";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
+import Navbar from "./components/layout/Navbar";
 
 import Container from "./components/layout/Container";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/Contact">Contact</Link>
-        <Link to="/Company">Company</Link>
-        <Link to="/NewProject">NewProject</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-hight">
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ custo/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/Contact">Contact</Link>
+      <Link to="/Company">Company</Link>
+      <Link to="/NewProject">NewProject</Link>
+    </div>
+  );
+}
+export default Navbar;
