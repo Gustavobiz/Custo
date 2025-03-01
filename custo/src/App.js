ad696cbef2388631c5210562a4885e7f414eea32
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-01 10:39:10 -0300

    fix: removendo link

--- custo/src/App.js
+++ custo/src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Navbar from "./components/layout/Navbar";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/Company" element={<Company />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/NewProject" element={<NewProject />} />
+          <Route path="/Projects" element={<Projects />} />
         </Routes>
       </Container>
       <Footer />
