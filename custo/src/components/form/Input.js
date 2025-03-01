bfdf81452c5d706a4cc6f8de78a4bb74d896c71d
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-01 11:56:51 -0300

    feat: input css

--- custo/src/components/form/Input.js
+++ custo/src/components/form/Input.js
@@ -1,2 +1,8 @@
-function Input() {}
+import styles from "./Input.module.css";
+function Input({type,text,name,placeholder,handleOnChange, value}) {
+    return(
+        <label htmlFor=""></label>
+        <input type=""></input>
+    )
+}
 export default Input;
