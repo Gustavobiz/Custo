578e17fcfb3b8a5cf6f246b4e6343062f0b87708
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-01 08:56:40 -0300

    feat: buttom css

--- custo/src/components/layout/LinkButtom.js
+++ custo/src/components/layout/LinkButtom.js
@@ -2,7 +2,7 @@ import styles from "./LinkButtom.module.css";
 import { Link } from "react-router-dom";
 function LinkButtom({ to, text }) {
   return (
-    <Link className={styles.bnt} to={to}>
+    <Link className={styles.btn} to={to}>
       {text}
     </Link>
   );
