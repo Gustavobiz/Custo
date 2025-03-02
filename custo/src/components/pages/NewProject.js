e42710c70e274e2c66f86e54d0f0673eb36ac652
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-01 21:50:28 -0300

    feat: create post

--- custo/src/components/pages/NewProject.js
+++ custo/src/components/pages/NewProject.js
@@ -1,6 +1,14 @@
 import styles from "./NewProject.module.css";
+import { useHistory } from "react-router-dom";
 import ProjectForm from "../project/ProjectForm";
 function NewProject() {
+  const history = useHistory();
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+    fetch("http://localhost:5000/projects");
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
